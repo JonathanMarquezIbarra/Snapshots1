3669e14575f8de8360a08a8ed0c88e98f03d0b4e
JonathanMarquezIbarra <jonathan.marquib@gmail.com>
2022-09-08 22:24:05 -0500

    Initial commit

---
+++ app/src/main/java/com/example/snapshots1/HomeAux.kt
@@ -0,0 +1,5 @@
+package com.example.snapshots1
+
+interface HomeAux {
+    fun goToTop()
+}
---
+++ app/src/main/java/com/example/snapshots1/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.snapshots1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.snapshots1.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 21
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    private lateinit var mActiveFragment: Fragment
+    private lateinit var mFragmentManager: FragmentManager
+
+    private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+    private var mFirebaseAuth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupAuth()
+        setupBottomNav()
+    }
+
+    private fun setupAuth() {
+        mFirebaseAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            val user = it.currentUser
+            if (user == null){
+                startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+                    //.setIsSmartLockEnabled(false) para deshabilitar el guardado de sesion anterior
+                    .setAvailableProviders(
+                        Arrays.asList(
+                            AuthUI.IdpConfig.EmailBuilder().build(),
+                            AuthUI.IdpConfig.GoogleBuilder().build())
+                    )
+                    .build(), RC_SIGN_IN)
+            }
+        }
+    }
+
+    private fun setupBottomNav(){
+        mFragmentManager = supportFragmentManager
+
+        val homeFragment = HomeFragment()
+        val addFragment = AddFragment()
+        val profileFragment = ProfileFragment()
+
+        mActiveFragment = homeFragment
+
+        mFragmentManager.beginTransaction()
+            .add(R.id.hostFragment, profileFragment, ProfileFragment::class.java.name)
+            .hide(profileFragment).commit()
+
+        mFragmentManager.beginTransaction()
+            .add(R.id.hostFragment, addFragment, AddFragment::class.java.name)
+            .hide(addFragment).commit()
+
+        mFragmentManager.beginTransaction()
+            .add(R.id.hostFragment, homeFragment, HomeFragment::class.java.name)
+            .commit()
+
+        mBinding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.action_home -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(homeFragment).commit()
+                    mActiveFragment = homeFragment
+                    true
+                }
+                R.id.action_add -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(addFragment).commit()
+                    mActiveFragment = addFragment
+                    true
+                }
+                R.id.action_profile -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(profileFragment).commit()
+                    mActiveFragment = profileFragment
+                    true
+                }
+                else -> false
+            }
+        }
+
+        mBinding.bottomNav.setOnItemReselectedListener{
+            when(it.itemId){
+                R.id.action_home -> (homeFragment as HomeAux).goToTop()
+            }
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mFirebaseAuth?.addAuthStateListener(mAuthListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mFirebaseAuth?.removeAuthStateListener(mAuthListener)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN){
+            if (resultCode == RESULT_OK){
+                Toast.makeText(this, "Bienvenido!!!", Toast.LENGTH_SHORT).show()
+            } else {
+                if (IdpResponse.fromResultIntent(data) == null){
+                    finish()
+                }
+            }
+        }
+    }
+}
